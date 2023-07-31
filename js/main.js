dd2986dec7983d9c982593776addae116d5f0135
Konis <hkn2007@naver.com>
2023-07-31 14:11:12 +0900

    update main.css, index.html and main.js

--- js/main.js
+++ js/main.js
@@ -25,18 +25,38 @@ window.addEventListener(
   _.throttle(function () {
     console.log(window.scrollY);
     if (window.scrollY > 500) {
+      // 배지 숨기기
+      // gsap.to(요소, 지속시간, 옵션);
       gsap.to(badgeEl, 0.6, {
         opacity: 0,
         display: "none",
       });
+      // 버튼 보이기!
+      gsap.to("#to-top", 0.2, {
+        x: 0,
+      });
     } else {
+      // 배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+      // 버튼 숨기기
+      gsap.to("#to-top", 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
+// _.throttle(함수, 시간) : 시간에 따라 함수가 실행되는 것을 제한한다.
+
+const toTopEl = document.querySelector("#to-top");
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
+
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
@@ -69,6 +89,17 @@ new Swiper(".promotion .swiper", {
   navigation: {
     prevEl: ".promotion .swiper-prev", // 이전 슬라이드 버튼 선택자
     nextEl: ".promotion .swiper-next", // 다음 슬라이드 버튼 선택자
+  },
+});
+new Swiper(".awards .swiper", {
+  // direction: "horizontal" : 기본값
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev", // 이전 슬라이드 버튼 선택자
+    nextEl: ".awards .swiper-next", // 다음 슬라이드 버튼 선택자
   },
 });
 
@@ -111,3 +142,16 @@ function floatingObject(selector, delay, size) {
 floatingObject(".floating1", 1, 15);
 floatingObject(".floating2", 0.5, 15);
 floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8, // 뷰포트의 0.8 지점에서 보여짐 여부 감시
+  })
+    .setClassToggle(spyEl, "show") // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당(필수)
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear(); // 2023
